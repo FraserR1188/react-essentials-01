506d82d3f0817f37358f502f8285f30b4afcf213
Robbie Fraser <fraserrobbie2@gmail.com>
2026-02-28 14:22:40 +0000

    add arrow functions to TabButton in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleSelect() {
-    console.log("Hello world!");
+  function handleSelect(selectedButton) {
+    console.log("Selected button:", selectedButton);
   }
 
   return (
@@ -28,11 +28,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
